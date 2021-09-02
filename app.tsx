dcb69c526743a895578fef297510dc78ceafa4a9
jlsnow301 <jlsnow.301@gmail.com>
2021-09-01 18:38:27 -0700

    dist -> docs

--- app.tsx
+++ app.tsx
@@ -1,6 +1,12 @@
-import React, { ComponentType } from 'react'
+import React, { ComponentType } from "react";
 
-export default function App({ Page, pageProps }: { Page: ComponentType<any>, pageProps: any }) {
+export default function App({
+  Page,
+  pageProps,
+}: {
+  Page: ComponentType<any>;
+  pageProps: any;
+}) {
   return (
     <main>
       <head>
@@ -8,5 +14,5 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
       </head>
       <Page {...pageProps} />
     </main>
-  )
+  );
 }
