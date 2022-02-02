b03c6f1c33ec0d3002a7341a5dd0addc62396c0f
Jeremiah <jlsnow301@protonmail.com>
2022-02-01 21:02:44 -0800

    Streamline

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./components/pages/Home";
-import { About } from "./components/pages/About";
+import { ModButton as Button } from "./components/elements/Button";
+import { Image } from "./components/elements/Image";
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <div className="app">
+      <Image source="./logo-cropped.png" />
+      <h1>Jeremiah Snow</h1>
+      Professional Details
+      <div className="app__buttons">
+        <Button download href="/jeremiah_snow_resume.pdf">
+          Download Resume - PDF
+        </Button>
+        <Button download href="/jeremiah_snow_resume.docx">
+          Download Resume - WORD
+        </Button>
+        <Button href="https://www.github.com/jlsnow301">
+          View Other Projects
+        </Button>
+      </div>
+    </div>
   );
 };
