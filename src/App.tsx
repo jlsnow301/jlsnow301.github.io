afb5eeb3cc8eba21adbcd9282a97d6e6caf75783
Jeremiah <jlsnow301@protonmail.com>
2022-02-05 00:02:08 -0800

    fixed clipping issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import GitHubCalendar from 'react-github-calendar';
 import "./components/css/app.css";
 
 export const App = () => {
+  
   return (
     <div className="app">
       <Image source="./logo-cropped.png" />
       <h1>Jeremiah Snow</h1>
       full stack web developer.
       <div className="app__buttons">
-        <Button download href="/jeremiah_snow_resume.pdf">
-          Download Resume - PDF
+      <Button download href="/jeremiah_snow_resume.docx">
+          Download Resume (WORD)
         </Button>
-        <Button download href="/jeremiah_snow_resume.docx">
-          Download Resume - WORD
+        <Button download href="/jeremiah_snow_resume.pdf">
+          Download Resume (PDF)
         </Button>
         <Button href="https://www.github.com/jlsnow301">
           View Other Projects
