3d156a835cc11ea85b5d989bd8e879cb6b665a27
Jeremiah <jlsnow301@protonmail.com>
2022-08-24 18:42:16 -0700

    more fixin'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()],
+  integrations: [],
   vite: {
     ssr: {
       external: ["svgo"],
