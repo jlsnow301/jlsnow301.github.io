98a3ef7722c7059f50451a66acbf2992ce5702fe
Jeremiah <jlsnow301@protonmail.com>
2022-08-24 18:30:06 -0700

    Converted to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image(), react()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
