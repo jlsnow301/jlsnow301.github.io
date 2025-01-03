ae31990175c00b80be3258d7394a0bb326132b6f
jlsnow301 <jlsnow301@pm.me>
2025-01-02 19:10:46 -0800

    version bump + host move

--- docs/astro/my-avatar.DYaBku9F.js
+++
@@ -1,1 +0,0 @@
-import{M as a}from"./my-avatar.CNQ7KZ8G.js";import"./utils.CBNs1GJq.js";import"./index.rE2EVDCa.js";import"./index.D9q_tMoa.js";import"./index.zkFWU35Z.js";export{a as MyAvatar};
--- docs/astro/theme-toggle.BWSaRAhj.js
+++
@@ -1,1 +0,0 @@
-import{T as p}from"./theme-toggle.BdMNJrDu.js";import"./utils.CBNs1GJq.js";import"./index.rE2EVDCa.js";export{p as ThemeToggle};
